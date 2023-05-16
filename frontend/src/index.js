4936a439a8e14dbaf25a22e6252263dc6b31ef86
Artturi1 <arttuurom@gmail.com>
2023-05-16 11:50:51 +0300

    commit

---
+++ frontend/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Films from "./pages/films";
+import Layout from "./pages/layout";
+import Actor from "./pages/actors";
+import Add from "./pages/search_films";
+
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/films" element={<Films />} />
+          <Route path="/actors" element={<Actor />} />¨
+          <Route path="/search_films" element={<Add />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
+  );
+ 
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+
+
---
+++ frontend/src/pages/films.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const Films = () => {
+    const [films,setfilms] = useState([]);
+
+    useEffect(() => {
+      const fetch = async () => {
+        try {
+          const response = await axios.get('http://localhost:8081/films');
+          console.log(response)
+          setfilms(response.data);
+        }
+        catch (err) {
+          console.log(err);
+        }
+      };
+  
+      fetch();
+    }, []);
+  
+  
+    return (
+      <section className="home" id="home">
+      <div className="max-width">
+        <div className="home-content">
+        <div className="App">
+        
+          <ul>
+            {films.map(film => (
+              <div key={film.title}>
+                <h2>{film.title}</h2>
+                <p>{film.description}</p>
+              </div>
+            ))}
+          </ul>
+        </div>
+        </div>
+      </div>
+      </section>
+      
+        
+  
+    );
+    }
+
+export default Films;
